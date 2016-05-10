ea40504f81d01614ff4ae86c69c1eae94039248e
Nicolas Galler <ngaller@gmail.com>
2016-05-10 10:12:12 -0400

    Create Change Activity Editor Label.js

---
+++ js/Change Activity Editor Label.js
@@ -0,0 +1,3 @@
+    require(['dojo/i18n!Sage/MainView/ActivityMgr/nls/ActivityEditor'], function (activityEditorStrings) {
+        Object.getPrototypeOf(activityEditorStrings).categoryText = 'Something else';
+    });
